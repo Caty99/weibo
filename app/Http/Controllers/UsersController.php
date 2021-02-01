908873d4e7f4ce76b612b7c41214568feedd317c
vagrant <vagrant@homestead>
2021-02-01 02:01:13 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 class UsersController extends Controller
 {
     public function create()
@@ -29,8 +30,22 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        Auth::login($user);
         session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
 
         return redirect()->route('users.show',[$user]);
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request,['name'=>'required|max:50','password'=>'required|confirmed|min:6']);
+        $user->update(['name'=>$request->name,'password'=>bcrypt($request->password),]);
+
+        return redirect()->route('user.show',$user->id);
     }
 }
