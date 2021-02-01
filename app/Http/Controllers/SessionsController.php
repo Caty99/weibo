cfba68685144d3ada02ff04b53d66054ce0e076a
vagrant <vagrant@homestead>
2021-02-01 03:49:15 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,27 +8,33 @@ use Illuminate\Support\Facades\Auth as FacadesAuth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
     }
 
-    public function store(Request $request)
+     public function store(Request $request)
     {
-        $credentials = $this->validate($request,[
-            'email' => 'required|email|max:255',
-            'password' => 'required'
-        ]);
+       $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
+       ]);
 
-        if (Auth::attempt($credentials,$request->has('remeber')))
-    {
-        session()->flash('success','欢迎回来!');
-        return redirect()->route('users.show',[Auth::user()]);//登录成功后的相关操作
-    }else{
-        session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
-        return redirect()->back()->withInput();
-        //登录失败后的相关操作
-        }
+       if (Auth::attempt($credentials, $request->has('remember'))) {
+           session()->flash('success', '欢迎回来！');
+           $fallback = route('users.show', Auth::user());
+           return redirect()->intended($fallback);
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back()->withInput();
+       }
     }
 
     public function destroy()
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,17 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
@@ -38,14 +49,27 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
-        return view('users.edit',compact('user'));
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
-        $this->validate($request,['name'=>'required|max:50','password'=>'required|confirmed|min:6']);
-        $user->update(['name'=>$request->name,'password'=>bcrypt($request->password),]);
+        $this->authorize('update', $user);
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
 
-        return redirect()->route('user.show',$user->id);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user);
     }
 }
